328af1cb92a7f7643097ed4ac42f14a2b82f06e9
Svb home <sergio.valverde.b@gmail.com>
2016-12-11 16:21:17 -0600

    Initial redux notes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import { createStore } from "redux";
 import expect from "expect";
 
 
-const counter = (state, action) => {
+const counter = (state = 0, action) => {
 
-	if (action.type === "INCREMENT"){
-		return state + 1;
+	if (typeof state === "undefined"){
+		return 0;
 	}
-	if (action.type === "DECREMENT"){
-		return state - 1;
+
+	switch (action.type) {
+		case "INCREMENT":
+			return state + 1;
+		case "DECREMENT":
+			return state - 1;
+		default:
+			return state;
 	}
 
 }
 
+//Tests to handle increments:
 expect(
 	counter(0, {type: "INCREMENT"})
 ).toEqual(1);
@@ -22,6 +29,7 @@ expect(
 	counter(1, {type: "INCREMENT"})
 ).toEqual(2);
 
+//Tests to handle decrements:
 expect(
 	counter(2, {type: "DECREMENT"})
 ).toEqual(1);
@@ -30,19 +38,45 @@ expect(
 	counter(1, {type: "DECREMENT"})
 ).toEqual(0);
 
+//Tests to handle unknown actions:
+expect(
+	counter(1, {type: "SOMETHING_ELSE"})
+).toEqual(1);
+
+//Tests to handle initial state:
+expect(
+	counter(undefined, {})
+).toEqual(0);
+
 console.log("Tests passed!");
 
 
 
-const store = createStore(counter, 0);
+const store = createStore(counter);
+
+store.dispatch({type: "INCREMENT"});
+console.log(store.getState());
+
 
 export default class App extends React.Component{
+
+	decrease(){
+		console.log("Decrease test");
+		store.dispatch({type: "DECREMENT"})
+		console.log(store.getState());
+	}
+	increase(){
+		console.log("Decrease test");
+		store.dispatch({type: "INCREMENT"})
+		console.log(store.getState());
+	}
 
 	render(){
 		return (
 			<div>
-				<h1>Counter: </h1>
-
+				<h1>Counter: {store.getState()}</h1>
+				<button onClick={this.decrease}>-</button>
+				<button onClick={this.increase}>+</button>
 			</div>
 		);
 	}
