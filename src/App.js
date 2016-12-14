b0339874922c47d09019a5ed0b1b6905efd96ec2
Svb home <sergio.valverde.b@gmail.com>
2016-12-13 21:02:01 -0600

    App.js modifications

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { createStore } from "redux";
 import expect from "expect";
 
 
-const counter = (state = 0, action) => {
+const counterReducer = (state = 0, action) => {
 
 	if (typeof state === "undefined"){
 		return 0;
@@ -22,61 +22,66 @@ const counter = (state = 0, action) => {
 
 //Tests to handle increments:
 expect(
-	counter(0, {type: "INCREMENT"})
+	counterReducer(0, {type: "INCREMENT"})
 ).toEqual(1);
 
 expect(
-	counter(1, {type: "INCREMENT"})
+	counterReducer(1, {type: "INCREMENT"})
 ).toEqual(2);
 
 //Tests to handle decrements:
 expect(
-	counter(2, {type: "DECREMENT"})
+	counterReducer(2, {type: "DECREMENT"})
 ).toEqual(1);
 
 expect(
-	counter(1, {type: "DECREMENT"})
+	counterReducer(1, {type: "DECREMENT"})
 ).toEqual(0);
 
 //Tests to handle unknown actions:
 expect(
-	counter(1, {type: "SOMETHING_ELSE"})
+	counterReducer(1, {type: "SOMETHING_ELSE"})
 ).toEqual(1);
 
 //Tests to handle initial state:
 expect(
-	counter(undefined, {})
+	counterReducer(undefined, {})
 ).toEqual(0);
 
 console.log("Tests passed!");
 
 
 
-const store = createStore(counter);
-
-store.dispatch({type: "INCREMENT"});
-console.log(store.getState());
+const store = createStore(counterReducer);
 
 
 export default class App extends React.Component{
 
+	constructor(props){
+		super(props);
+
+		this.state = {
+			theCounter: store.getState()
+		}
+
+		store.subscribe(() => {
+			this.setState({theCounter: store.getState()})
+		});
+	}
+
 	decrease(){
-		console.log("Decrease test");
 		store.dispatch({type: "DECREMENT"})
-		console.log(store.getState());
 	}
 	increase(){
-		console.log("Decrease test");
 		store.dispatch({type: "INCREMENT"})
-		console.log(store.getState());
 	}
 
 	render(){
 		return (
 			<div>
-				<h1>Counter: {store.getState()}</h1>
-				<button onClick={this.decrease}>-</button>
-				<button onClick={this.increase}>+</button>
+				<h1>Counter: {this.state.theCounter}</h1>
+				<button onClick={this.decrease.bind(this)}>-</button>
+				<button onClick={this.increase.bind(this)}>+</button>
 			</div>
 		);
 	}
